7df4b02524f5e39b98723b286db8c773509779a4
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-11-22 07:36:00 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,10 @@
+# https://beta.openai.com/account/api-keys
+# Abrir cuenta con gmail
 API_KEY_GPT3 = "************************"
+
+# https://web.telegram.org
+# @BotFather
+# /newbot
 TOKEN_TELEGRAM="**************************"
 
 
